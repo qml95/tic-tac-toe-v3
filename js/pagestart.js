6e26e1644f074e5b7512e36cf25db0bbbd036611
qml95 <quentin.morino.laudrin@gmail.com>
2017-11-17 17:48:04 +0100

    begining the module Pattern for pagestart.js

--- js/pagestart.js
+++ js/pagestart.js
@@ -1,20 +1,23 @@
-(function() {
-  var pageDeJeu = document.getElementById('board');
-  pageDeJeu.style.display = "none";
-  var pageStartElt = document.createElement('div');
-  pageStartElt.id = "start";
-  pageStartElt.setAttribute('class', 'screen screen-start');
-  pageStartElt.innerHTML = `
-  <header>
-    <h1>
-      Tic Tac Toe
-    </h1>
-    <a href="#" class="button">
-      Start game
-    </a>
-  </header>
-  `;
-  console.log(pageStartElt);
-  var bodyElt = document.querySelector('body');
-  bodyElt.appendChild(pageStartElt);
+var board = (function() {
+  return {
+    home:function() {
+      var pageStartElt = document.createElement('div');
+      pageStartElt.id = "start";
+      pageStartElt.setAttribute('class', 'screen screen-start');
+      pageStartElt.innerHTML = `
+      <header>
+        <h1>
+          Tic Tac Toe
+        </h1>
+        <a href="#" class="button">
+          Start game
+        </a>
+      </header>
+      `;
+      var bodyElt = document.querySelector('body');
+      bodyElt.appendChild(pageStartElt);
+    }
+  };
 }());
+board.home();
+// board.startGame();

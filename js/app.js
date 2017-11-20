f238616ee216ac553bec8d693b679d913846160b
qml95 <quentin.morino.laudrin@gmail.com>
2017-11-20 16:12:28 +0100

    fin de la function game

--- js/app.js
+++ js/app.js
@@ -45,32 +45,32 @@ function startGame() {
       <li class="players player2"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="43" viewBox="0 0 42 43" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-718.000000, -60.000000)" fill="#000000"><g transform="translate(739.500000, 81.500000) rotate(-45.000000) translate(-739.500000, -81.500000) translate(712.000000, 54.000000)"><path d="M30 30.1L30 52.5C30 53.6 29.1 54.5 28 54.5L25.5 54.5C24.4 54.5 23.5 53.6 23.5 52.5L23.5 30.1 2 30.1C0.9 30.1 0 29.2 0 28.1L0 25.6C0 24.5 0.9 23.6 2 23.6L23.5 23.6 23.5 2.1C23.5 1 24.4 0.1 25.5 0.1L28 0.1C29.1 0.1 30 1 30 2.1L30 23.6 52.4 23.6C53.5 23.6 54.4 24.5 54.4 25.6L54.4 28.1C54.4 29.2 53.5 30.1 52.4 30.1L30 30.1Z"/></g></g></g></svg></li>
     </ul>
   </header>
-  <ul class="boxes" id="plateau" onclick="game()">
-    <li class="box" id="one"></li>
-    <li class="box" id="two"></li>
-    <li class="box" id="three"></li>
-    <li class="box" id="four"></li>
-    <li class="box" id="five"></li>
-    <li class="box" id="six"></li>
-    <li class="box" id="seven"></li>
-    <li class="box" id="eight"></li>
-    <li class="box" id="nine"></li>
+  <ul class="boxes">
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
+    <li class="box" onclick="game(this)"></li>
   </ul>
   `;
   bodyElt.appendChild(pageStartGameElt);
   var joueur1 = document.getElementsByClassName('players');
-
   joueur1[0].classList.remove('player1');
   joueur1[0].classList.add('active');
   joueur1[0].id = 'player1';
 }
 // fin de la fonction startGame
-
-function game(){
+//debut de la fonction game pour le jeu
+function game(tour){
   var joueur1 = document.getElementById('player1');
   if (joueur1.classList.contains('active')) {
     joueur1.classList.remove('active');
     joueur1.classList.add('player1');
+    tour.classList.add('box-filled-1')
     var joueur2 = document.getElementsByClassName('players');
     joueur2[1].classList.remove('player2');
     joueur2[1].classList.add('active');
@@ -79,11 +79,11 @@ function game(){
     var joueur2 = document.getElementById('player2');
     joueur2.classList.remove('active');
     joueur2.classList.add('player1');
+    tour.classList.add('box-filled-2')
     var joueur1 = document.getElementsByClassName('players');
     joueur1[0].classList.remove('player1');
     joueur1[0].classList.add('active');
     joueur1[0].id = 'player1';
   }
 
-
-}
+}// fin de la fonction game
